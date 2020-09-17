35523ec1008e1f43a33be2e5e787e784709f7ad9
Hiromi Takahashi <hiromix222@gmail.com>
2020-09-17 20:23:05 +0900

    modify JPA

--- src/main/java/com/example/ddd/BookRepository.java
+++ src/main/java/com/example/ddd/BookRepository.java
@@ -1,9 +1,9 @@
 package com.example.ddd;
 
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-//@Repository
+@Repository
 
 public interface BookRepository
         // extends JpaRepository<Book,String>
